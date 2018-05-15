413c325b49a6d94f888434de465e4a40efcaa2cf
Eiji <morinokumakamesan@gmail.com>
2018-05-15 22:57:17 +0900

    詳細ページのmapをvue2-google-mapsで表示

--- src/pages/todo-detail/TodoDetail.vue
+++ src/pages/todo-detail/TodoDetail.vue
@@ -6,12 +6,35 @@
 <template>
 <v-ons-page>
   <navbar navType="back" msg="todo"></navbar>
+  <v-ons-popover cancelable
+    :visible.sync="popoverVisible"
+    :target="popoverTarget"
+    :direction="popoverDirection"
+    :cover-target="coverTarget"
+  >
+    <!-- aaaaaaaa -->
+    <center>{{ infos.content }}</center>
+  </v-ons-popover>
   <div class="page-content">
     <h3 class="page-title">{{ infos.content }}の所在地！</h3>
     <!-- <div style="margin: 10px" v-for="(value, key) in infos" v-bind:key="value">{{ key }}: {{ value }}</div> -->
   </div>
   <div align="center">
-    <iframe width="80%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" v-bind:src=MapSrc></iframe>
+    <GmapMap
+      :center="{lat:infos.lat, lng:infos.lng}"
+      :zoom="16"
+      map-type-id="roadmap"
+      style="width: 80%; height: 400px"
+    >
+      <GmapMarker
+        :position="{lat:infos.lat, lng:infos.lng}"
+        :clickable="true"
+        :draggable="true"
+        @click="showPopover($event, 'up')"
+      />
+    </GmapMap>
+
+    <!-- <iframe width="80%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" v-bind:src=MapSrc></iframe> -->
   </div>
 </v-ons-page>
 </template>
@@ -26,6 +49,33 @@ export default {
   },
   props: {
     infos: [],
+  },
+  methods: {
+    Alert() {
+      this.$ons.notification.alert('場所を入力してください');
+    },
+    toggleBounce() {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+    },
+    showPopover(event, direction, coverTarget = false) {
+      console.log(event)
+      this.popoverTarget = event.Ia;
+      this.popoverDirection = direction;
+      this.coverTarget = coverTarget;
+      this.popoverVisible = true;
+    },
+  },
+  data() {
+    return {
+      popoverVisible: false,
+      popoverTarget: null,
+      popoverDirection: 'up',
+      coverTarget: false,
+    }
   },
   computed: {
     MapSrc() {
